ee12ab493ec13415ff91517aca38544d1115a7cb
sgt-dope <sgtdopeaf@gmail.com>
2021-08-17 17:46:48 +0000

    added authentication

--- fetch.py
+++ fetch.py
@@ -1,7 +1,11 @@
 import requests
+import os
+from requests.auth import HTTPBasicAuth
+
 
 get_url = requests.get(
-    url="https://fetch-url-ff.herokuapp.com"
+    url="https://fetch-url-ff.herokuapp.com/",
+    auth = HTTPBasicAuth(os.environ.get("AUTH_USERNAME"), os.environ.get("AUTH_PASSWORD"))
 )
 
 keycloak_url = get_url.json()[0]["url"]
